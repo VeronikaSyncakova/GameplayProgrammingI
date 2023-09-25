e4949cc0a989409f085ce6900d23158ea394eeee
(Student  C00288340) Veronika Syncakova <C00288340@setu.ie>
2023-09-25 11:05:30 +0100

    need to finish launch code

--- PRACTICAL_02/src/main.cpp
+++ PRACTICAL_02/src/main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <cstdlib>
+#include<ctime>
+#include <string>
+
 
 using namespace std;
 
@@ -30,6 +34,8 @@ struct Missile
 	WarHead payload;
 	Coordinates coordinates;
 	Target target;
+	//launch code
+	string const LAUNCH_CODE="816 80085";
 
 	bool armed;
 
@@ -47,7 +53,7 @@ struct Missile
 		coordinates.y += 2;
 	}
 
-	void selectWarhead()
+	void selectWarhead() //select warhead type
 	{
 		int warhead;
 		cout<<"Select warhead\n*Explosive-0\n*Nuclear-1\n";
@@ -61,46 +67,64 @@ struct Missile
 			payload = WarHead::NUCLEAR;
 			cout<<"You have chosen: nuclear\n";
 		}
-		else{
+		else{ //error check for invalid input
 			cout<<"Please select again !\n";
 			selectWarhead();
 		}
 	}
 
-	void acquireTarget()
+	void acquireTarget() //set the shooting target
 	{
 		int x,y,ans;
-		cout<<"\nSelect target position (0-6)\n";
+		cout<<"\nSelect target position (0-6)\n"; //6x6
 		cout<<"x: ";
 		cin>>x;
 		cout<<"y: ";
 		cin>>y;
-		cout<<"You have chosen\n["<<x<<" ,"<<y<<" ]\n*Change the target-0\n*Fire-1\n";
+		cout<<"You have chosen\n["<<x<<" ,"<<y<<" ]\n*Change the target-0\n*Fire-1\n"; //options 
 		cin>>ans;
-		if(ans==0)
+		if(ans==0) //user wants to change the target
 		{
 			acquireTarget();
 		}
-		else
+		else //user fires 
 		{
 			coordinates.x=x;
 			coordinates.y=y;
 		}
 	}
 
-	bool fire()
+	void  launchCode()
+	{ /*
+		string ans="";
+			cout<<"Enter launch code: ";
+			cin>>static_cast<string>(ans);
+			while(!(ans==LAUNCH_CODE))
+			{
+				cout<<"\nEnter launch code or press 0 to view the launch code: ";
+				cin>>static_cast<string>(ans);
+
+				if (ans=="1")
+				{
+					cout<<"\nLaunch code: "<<LAUNCH_CODE<<"\n";
+				}
+			}
+			*/
+	}
+
+	bool fire() //checks if it was possible to fire or if the user had bad luck
 	{
 		int chance=rand()%11; //chance that the weather will be suitable for attack
 		bool fire=false;
 		if(payload==EXPLOSIVE)
 		{
-			if(chance>5)
+			if(chance>5) //lower chance
 			{
 				fire=true;
 			}
 		}
 		else{
-			if(chance>3)
+			if(chance>3) //higher chance 
 			{
 				fire=true;
 			}
@@ -108,7 +132,7 @@ struct Missile
 		return fire;
 	}
 
-	bool checkCollision()
+	bool checkCollision() //check collision between missiole and target
 	{
 		if( target.coordinates.x==coordinates.x && target.coordinates.y==coordinates.y)
 		{
@@ -118,8 +142,18 @@ struct Missile
 			return false;
 		}
 	}
+	bool viable() //checks if the coordinates are within the playing area
+	{
+		if (coordinates.x<0 || coordinates.x>6 || coordinates.y<0 || coordinates.y>6) //coordinates outside of the area
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
 };
-
 
 
 int main()
@@ -128,19 +162,25 @@ int main()
 	Enemy *e = new Enemy();
 
 	// Set Enemy Position / Target
-	e->coordinates.x = 2;
-	e->coordinates.y = 2;
-	
-	while( e->alive)
-	{
-		// Print Enemy Position
-		cout<<"//////////////////////////////////\n";
-		cout << "Print Enemy Position" << endl;
-		e->coordinates.print();
-		cout<<"//////////////////////////////////\n";
+	srand(time(nullptr));
+	e->coordinates.x = rand()%7;
+	e->coordinates.y = rand()%7;
 
-		// Create a new Missile
-		Missile *m = new Missile();
+	// Print Enemy Position
+	cout<<"//////////////////////////////////\n";
+	cout << "Print Enemy Position" << endl;
+	e->coordinates.print();
+	cout<<"//////////////////////////////////\n";
+
+	// Create a new Missile
+	Missile *m = new Missile();
+
+	//launch code
+	cout<<"Launch code: "<<m->LAUNCH_CODE<<"\n";
+	
+	
+	while( e->alive) //keep playing while the enemy is not destroyed
+	{
 
 		//Select warhead
 		m->selectWarhead();
@@ -151,7 +191,7 @@ int main()
 		m->acquireTarget();
 
 		//Fire
-		while(!(m->fire()))
+		while(!(m->fire())) //if the weather is bad, repeat until you can shoot
 		{
 			int ans;
 			cout<<"Bad weather, couldn't attack.\n*Press 1 to fire again\n";
@@ -169,7 +209,14 @@ int main()
 			e->alive=false;
 		}
 		else{
-			cout<<"Try again!\n\n";
+			if (m->viable())
+			{
+				cout<<"Target outside of the board!\nThe board is 6x6\n\n";
+				//m->acquireTarget();
+			}
+			else{
+				cout<<"Missed, try again!\n\n";
+			}
 		}
 	}
 
